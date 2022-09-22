ee3795ea94427bf2fb2e7314706e05cbd0e00087
Kephren <kephren.delannay.sampany@gmail.com>
2022-09-22 17:00:16 -0400

    Corrected to pass the tests

--- exercice.py
+++ exercice.py
@@ -4,19 +4,16 @@
 
 def order(values: list = None) -> list:
     if values is None:
-        values = [float(input("Entrer un nombre ")) for i in range(10)]
-        values.sort()
-    return values
+        values = [(input("Entrer une valeur ")) for i in range(10)]
+    return sorted(values)
+
 
 
 def anagrams(words: list = None) -> bool:
     l1, l2 = [], []
     if words is None:
-        words = [input('Veuillez entrer la chaine de caractère') for i in range(2)]
-        l1, l2 = list(words[0]), list(words[1])
-        l1.sort()
-        l2.sort()
-    return l1 == l2
+        words = [input('Veuillez entrer la chaine de caractère \n') for i in range(2)]
+    return sorted(list(words[0])) == sorted(list(words[1]))
 
 
 def contains_doubles(items: list) -> bool:
@@ -29,12 +26,12 @@ def contains_doubles(items: list) -> bool:
 def best_grades(student_grades: dict) -> dict:
     # TODO: Retourner un dictionnaire contenant le nom de l'étudiant ayant la meilleure moyenne ainsi que sa moyenne
     g = list(student_grades.values())[0]
-    best = {list(student_grades.keys())[0] : g}
     best_moy = sum(g) / len(g)
+    best = {list(student_grades.keys())[0] : best_moy}
     for key, val in student_grades.items():
         moy = sum(val) / len(val)
         if moy > best_moy:
-            best = {key : val}
+            best = {key : moy}
     return best
 
 
@@ -43,9 +40,10 @@ def frequence(sentence: str) -> dict:
     #       Retourner le tableau de lettres
     result = {}
     for lettre in sentence:
-        if sentence.count(lettre) >= 5 and lettre.isalnum():
-            result[lettre] = sentence.count(lettre)
-
+        result[lettre] = sentence.count(lettre)
+    for key, val in result.items():
+        if val > 5 and key.isalnum():
+            print(f'La lettre {key} revient {val} fois')
     return {k: v for k, v in sorted(result.items(), key=lambda item:item[1], reverse=True)}
 
 
@@ -65,27 +63,27 @@ def print_recipe(ingredients) -> None:
 
 
 def main() -> None:
-    print(f"On essaie d'ordonner les valeurs...")
-    print(order())
-
-    print(f"On vérifie les anagrammes...")
-    print(anagrams())
-
-    my_list = [3, 3, 5, 6, 1, 1]
-    print(f"Ma liste contient-elle des doublons? {contains_doubles(my_list)}")
-
-    grades = {"Bob": [90, 65, 20], "Alice": [85, 75, 83]}
-    best_student = best_grades(grades)
-    print(f"{list(best_student.keys())[0]} a la meilleure moyenne: {list(best_student.values())[0]}")
-
-    sentence = "bonjour, je suis une phrase. je suis compose de beaucoup de lettre. oui oui"
+    # print(f"On essaie d'ordonner les valeurs...")
+    # print(order())
+    #
+    # print(f"On vérifie les anagrammes...")
+    # print(anagrams())
+    #
+    # my_list = [3, 3, 5, 6, 1, 1]
+    # print(f"Ma liste contient-elle des doublons? {contains_doubles(my_list)}")
+    #
+    # grades = {"Bob": [90, 65, 20], "Alice": [85, 75, 83]}
+    # best_student = best_grades(grades)
+    # print(f"{list(best_student.keys())[0]} a la meilleure moyenne: {list(best_student.values())[0]}")
+    #
+    sentence =  "J'adore mon baccalaureat en genie informatique/logiciel"#"bonjour, je suis une phrase. je suis compose de beaucoup de lettre. oui oui"
     print(frequence(sentence))
-
-    print("On enregistre les recettes...")
-    recipes = get_recipes()
-
-    print("On affiche une recette au choix...")
-    print_recipe(recipes)
+    #
+    # print("On enregistre les recettes...")
+    # recipes = get_recipes()
+    #
+    # print("On affiche une recette au choix...")
+    # print_recipe(recipes)
 
 
 if __name__ == '__main__':
